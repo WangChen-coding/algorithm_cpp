86c214a45ce240bea06c1087d67b2d1f692eb65f
ChenWang <1009446554@qq.com>
2021-09-18 22:24:59 +0800

    9.18

--- class21/Code03_CoinsWayNoLimit/Code03_CoinsWayNoLimit.cpp
+++ class21/Code03_CoinsWayNoLimit/Code03_CoinsWayNoLimit.cpp
@@ -21,13 +21,39 @@ int coinway(vector<int>& arr, int aim) {
 int coinway2(vector<int>& arr, int aim) {
 	int N = arr.size();
 	vector<vector<int>>dp(N + 1, vector<int>(aim + 1, 0));
+	dp[N][0] = 1;
+	for (int i = N - 1; i >= 0; i--) {
+		for (int j = 0; j <= aim; j++) {
+			int ways = 0;
+			for (int zhang = 0; zhang * arr[i] <= j; zhang++) {
+				ways += dp[i + 1][j - (zhang * arr[i])];
+			}
+			dp[i][j] = ways;
+		}
+	}
+	return dp[0][aim];
+}
+int coinway3(vector<int>& arr, int aim) {
+	int N = arr.size();
+	vector<vector<int>>dp(N + 1, vector<int>(aim + 1, 0));
+	dp[N][0] = 1;
+	for (int i = N - 1; i >= 0; i--) {
+		for (int j = 0; j <= aim; j++) {
+			dp[i][j] = dp[i + 1][j];
+			if (j - arr[i] >= 0) {
+				dp[i][j] += dp[i][j - arr[i]];
+			}
+		}
+	}
+	return dp[0][aim];
 }
 
 int main()
 {
 	vector<int>m = { 1,3,1,9,8,1,3,4,5,2,6,1,8,8,4,8 };
 	cout << coinway(m, 10) << endl;
-	cout << coinway(m, 10) << endl;
+	cout << coinway2(m, 10) << endl;
+	cout << coinway3(m, 10) << endl;
 	return 0;
 }
 
