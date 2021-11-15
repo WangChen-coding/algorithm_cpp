01c71ba85953f9778e31d5ef04d0c43b0b13d500
ChenWang <1009446554@qq.com>
2021-11-15 16:56:47 +0800

    11.15

---
+++ class22/Code03_SplitNumber/Code03_SplitNumber.cpp
@@ -0,0 +1,81 @@
+﻿
+#include <iostream>
+#include<vector>
+using namespace std;
+
+int process(int pre, int rest) {
+    if (rest == 0) {
+        return 1;
+    }
+    if (pre > rest) {
+        return 0;
+    }
+    int ways = 0;
+    for (int first = pre; first <= rest; first++) {
+        ways += process(first, rest - first);
+    }
+    return ways;
+}
+
+int ways(int n) {
+    if (n < 0) {
+        return 0;
+    }
+    if (n == 1) {
+        return 1;
+    }
+    return process(1, n);
+}
+
+int dp1(int n) {
+    if (n <= 0) {
+        return 0;
+    }
+    if (n == 1) {
+        return 1;
+    }
+    vector<vector<int>>dp(n + 1, vector<int>(n + 1, 0));
+    for (int pre = 0; pre <= n; pre++) {
+        dp[pre][0] = 1;
+        dp[pre][pre] = 1;
+    }
+    for (int pre = n - 1; pre > 0; pre--) {
+        for (int rest = pre; rest <= n; rest++) {
+            int ways = 0;
+            for (int first = pre; first <= rest; first++) {
+                ways += dp[first][rest - first];
+            }
+            dp[pre][rest] = ways;
+        }
+    }
+    return dp[1][n];
+}
+
+int dp2(int n) {
+    if (n <= 0) {
+        return 0;
+    }
+    if (n == 1) {
+        return 1;
+    }
+    vector<vector<int>>dp(n + 1, vector<int>(n + 1, 0));
+    for (int pre = 0; pre <= n; pre++) {
+        dp[pre][0] = 1;
+        dp[pre][pre] = 1;
+    }
+    for (int pre = n - 1; pre > 0; pre--) {
+        for (int rest = pre; rest <= n; rest++) {
+            dp[pre][rest] = dp[pre + 1][rest];
+            dp[pre][rest] += dp[pre][rest - pre];
+        }
+    }
+    return dp[1][n];
+}
+
+int main()
+{
+    cout << ways(4) << endl;
+    cout << dp1(4) << endl;
+    cout << dp2(4) << endl;
+}
+
